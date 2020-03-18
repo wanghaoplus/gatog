44f797706bb607854aae5e96a80320ce9a7d9a1c
wanghaoplus <418816179@qq.com>
2020-03-18 23:58:34 +0800

    更新debug文件夹

---
+++ debug/tmp.py
@@ -0,0 +1,23 @@
+
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+
+class cat(object):
+    name = "tmp"
+    age = 22
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __getattribute__(self, attr):
+        try:
+            return object.__getattribute__(self, attr)
+        except AttributeError:
+            return print("have no attribute of %s" % attr)
+
+
+awang = cat("wanghao", 31)
+print(awang.name)
+print(awang.gender)
